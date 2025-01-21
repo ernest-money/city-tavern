2a04e977898b9b5c1004eff1b01d38eb4fa39d7a
bennyhodl <ben@bitcoinbay.foundation>
2025-01-21 11:09:50 -0500

    DDK transport

---
+++ src/transport.rs
@@ -0,0 +1,49 @@
+use std::sync::Arc;
+
+use crate::{routed_message::RoutedMessage, CityTavern};
+use bitcoin::secp256k1::PublicKey;
+use ddk::DlcDevKitDlcManager;
+use ddk::{Oracle, Storage, Transport};
+use tokio::sync::watch;
+
+#[async_trait::async_trait]
+impl Transport for CityTavern {
+    fn name(&self) -> String {
+        "city-tavern".to_string()
+    }
+
+    fn public_key(&self) -> PublicKey {
+        self.public_key()
+    }
+
+    async fn start<S: Storage, O: Oracle>(
+        &self,
+        mut stop_signal: watch::Receiver<bool>,
+        manager: Arc<DlcDevKitDlcManager<S, O>>,
+    ) -> Result<(), anyhow::Error> {
+        let listen_handle = self.listen(stop_signal.clone());
+
+        let process_handle = self.process_messages(stop_signal.clone());
+
+        // Wait for either task to complete or stop signal
+        tokio::select! {
+            _ = stop_signal.changed() => Ok(()),
+            res = listen_handle => res?,
+            res = process_handle => res?,
+        }
+    }
+
+    async fn send_message(&self, peer_id: PublicKey, msg: dlc_messages::Message) {
+        let routed_msg = RoutedMessage {
+            to: peer_id,
+            from: self.public_key(),
+            msg_type: crate::routed_message::OFFER_TYPE,
+            message: msg,
+        };
+        self.midnight_rider.send_message(routed_msg, peer_id);
+    }
+
+    async fn connect_outbound(&self, peer_id: PublicKey, addr: &str) {
+        let _ = self.connect_to_peer(peer_id, addr).await;
+    }
+}
